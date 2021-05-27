f57bd161dc800c25a295a82ae76d378d463a1bc0
Matas Tunkevičius <m.tunkevicius@gmail.com>
2021-05-27 16:50:28 +0300

    reorganized VS proj and made shaders compile on start

---
+++ AppVulkan/Utils/ShaderMan.cpp
@@ -0,0 +1,25 @@
+#include "ShaderMan.h"
+#include <windows.h>
+
+void ShaderMan::CompileShaders()
+{
+	STARTUPINFOA si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	//create new process and launch a shell/python? script to compile the shaders
+	char path[64] = "py Shaders\\compile_shaders.py\0";
+	if (!CreateProcessA(NULL, path, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+	{
+		//should keep running but cancel shader compilation
+		printf("CreateProcess failed (%d).\n", GetLastError());
+		return;
+	}
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	// Close process and thread handles. 
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+}
---
+++ AppVulkan/Utils/ShaderMan.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<iostream>
+
+//TODO: make the validator portable or the path better
+class ShaderMan
+{
+public:
+	ShaderMan();
+
+	// Compile all shaders in Shader directory, log any errors
+	// Uses windows api to create a new process and call compilation script
+	// but waits for process to finish
+	static void CompileShaders();
+	// Same as CompileShaders but async, must call WaitForCompile to make sure everything is compiled before using shaders
+	void CompileShadersAsync();//TODO
+	void WaitForCompile();
+
+	~ShaderMan();
+};
+
